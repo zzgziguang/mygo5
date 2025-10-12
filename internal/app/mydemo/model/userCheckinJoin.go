d2f9c8e6e5f1f862224a4b0113815fac7528e93d
zzg <zzgzhaoziguang@qq.com>
2025-10-12 14:36:22 +0800

    add UserCheckinJoin.JoinTimeStr,CreateAtStr, UpdateAtStr

--- internal/app/mydemo/model/userCheckinJoin.go
+++ internal/app/mydemo/model/userCheckinJoin.go
@@ -3,19 +3,23 @@ package model
 import "time"
 
 type UserCheckinJoin struct {
-	Id       int       `json:"id" gorm:"column:id" mapstructure:"id"`
-	Uid      int       `json:"uid" gorm:"column:uid" mapstructure:"uid"`
-	Cid      int       `json:"cid" gorm:"column:cid" mapstructure:"cid"`
-	JoinTime *time.Time `json:"joinTime" gorm:"column:joinTime" mapstructure:"joinTime"`
-	CreateAt *time.Time `json:"createAt" gorm:"column:createAt" mapstructure:"createAt"`
-	UpdateAt *time.Time `json:"updateAt" gorm:"column:updateAt" mapstructure:"updateAt"`
-	Status   int       `json:"status" gorm:"column:status" mapstructure:"status"`
+	Id          int        `json:"id" gorm:"column:id" mapstructure:"id"`
+	Uid         int        `json:"uid" gorm:"column:uid" mapstructure:"uid"`
+	Cid         int        `json:"cid" gorm:"column:cid" mapstructure:"cid"`
+	JoinTime    *time.Time `json:"joinTime" gorm:"column:join_time" mapstructure:"-"`
+	JoinTimeStr string     `json:"-" mapstructure:"joinTime"`
+	CreateAt    *time.Time `json:"createAt" gorm:"column:create_at" mapstructure:"-"` //mapstructure:"-"
+	CreateAtStr string     `json:"-" mapstructure:"createAt"`
+	UpdateAt    *time.Time `json:"updateAt" gorm:"column:update_at" mapstructure:"-"`
+	UpdateAtStr string     `json:"-" mapstructure:"updateAt"` //json"-"表示去掉这个字段
+	Status      int        `json:"status" gorm:"column:status" mapstructure:"status"`
 }
 
 const (
 	JoinStatusDelete int = 0 //删除
 	JoinStatusNormal int = 1 //正常
 )
-func (UserCheckinJoin)TableName string{
+
+func (UserCheckinJoin) TableName() string {
 	return "user_checkin_join"
 }
