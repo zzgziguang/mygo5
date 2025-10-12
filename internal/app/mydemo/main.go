1667f11927efd33f19c197a4e820a85f244ebc95
zzg <zzgzhaoziguang@qq.com>
2025-10-12 14:22:17 +0800

    添加了添加用户打卡信息，通过post uid和cid，可添加join表和record表信息， 获取用户打卡信息代码，通过uidcid获取

--- internal/app/mydemo/main.go
+++ internal/app/mydemo/main.go
@@ -54,6 +54,13 @@ func main() {
 
 	r.POST("/api/checkin/add", controller.AddCheckinHandler)   //添加checkin
 	r.GET("/api/checkin/all", controller.GetCheckinHandlerAll) //查询Checkin，根据id排序
+
+	//打卡信息
+	r.POST("/api/userCheckin/add", controller.AddUserCheckinHandler)
+	//获取用户每日打卡
+	r.POST("/api/userCheckin/get", controller.GetUserCheckinRecordHandler)
+	//
+	//r.POST("/api/userCheckin/getuid", controller.GetUserCheckinJoinRecordByuid)
 	// 启动服务器
 	log.Println("服务器启动在 :8080 端口")
 	log.Fatal(r.Run(":8080"))
