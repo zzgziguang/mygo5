f773d84ca9dedfb2c7b0ee057e19cd42f687b065
zzg <zzgzhaoziguang@qq.com>
2025-10-12 14:44:17 +0800

    add userCheckinRecord daomysql

---
+++ internal/app/mydemo/dao/mysql/userCheckinJoin.go
@@ -0,0 +1,37 @@
+package mysql
+
+import (
+	"demo1/internal/app/mydemo/model"
+
+	"gorm.io/gorm"
+)
+
+// 查询表
+func GetUserCheckinJoin(uid int, cid int) (userCheckinJoin *model.UserCheckinJoin, err error) {
+	err = DB.Where("uid=? and cid =? and status=?", uid, cid, model.JoinStatusNormal).First(&userCheckinJoin).Error
+	if err != nil {
+		if err == gorm.ErrRecordNotFound { //没查到数据返回空
+			return nil, nil
+		}
+		return nil, err
+	}
+	return userCheckinJoin, nil
+}
+
+// 根据uid查询表
+func GetUserCheckinJoinByuid(uid int) (userCheckinJoinByuid []*model.UserCheckinJoin, err error) {
+	err = DB.Where("uid=? and status=?", uid, model.JoinStatusNormal).Find(&userCheckinJoinByuid).Error
+	if err != nil {
+		if err == gorm.ErrRecordNotFound { //没查到数据返回空
+			return nil, nil
+		}
+		return nil, err
+	}
+	return userCheckinJoinByuid, nil
+}
+
+// 添加表
+func AddUserCheckinJoin(newUserCheckinJoin *model.UserCheckinJoin) (err error) {
+	err = DB.Create(&newUserCheckinJoin).Error
+	return
+}
