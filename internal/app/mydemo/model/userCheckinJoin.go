60abdb10a77a882e7f702e70d16ac11788a6ff8d
zzg <zzgzhaoziguang@qq.com>
2025-10-12 18:48:55 +0800

    add gorm:"-" of model usercheckinjoin

--- internal/app/mydemo/model/userCheckinJoin.go
+++ internal/app/mydemo/model/userCheckinJoin.go
@@ -7,11 +7,11 @@ type UserCheckinJoin struct {
 	Uid         int        `json:"uid" gorm:"column:uid" mapstructure:"uid"`
 	Cid         int        `json:"cid" gorm:"column:cid" mapstructure:"cid"`
 	JoinTime    *time.Time `json:"joinTime" gorm:"column:join_time" mapstructure:"-"`
-	JoinTimeStr string     `json:"-" mapstructure:"joinTime"`
+	JoinTimeStr string     `json:"-" gorm:"-" mapstructure:"joinTime"`
 	CreateAt    *time.Time `json:"createAt" gorm:"column:create_at" mapstructure:"-"` //mapstructure:"-"
-	CreateAtStr string     `json:"-" mapstructure:"createAt"`
+	CreateAtStr string     `json:"-" gorm:"-" mapstructure:"createAt"`
 	UpdateAt    *time.Time `json:"updateAt" gorm:"column:update_at" mapstructure:"-"`
-	UpdateAtStr string     `json:"-" mapstructure:"updateAt"` //json"-"表示去掉这个字段
+	UpdateAtStr string     `json:"-" gorm:"-" mapstructure:"updateAt"` //json"-"表示去掉这个字段
 	Status      int        `json:"status" gorm:"column:status" mapstructure:"status"`
 }
 
