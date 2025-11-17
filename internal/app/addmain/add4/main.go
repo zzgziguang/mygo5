02a3fae86c8e8e6417594700fb0bafb42ff25eb6
zzg <zzgzhaoziguang@qq.com>
2025-11-18 01:12:05 +0800

    将csv变为txt

--- internal/app/addmain/add4/main.go
+++ internal/app/addmain/add4/main.go
@@ -2,9 +2,8 @@ package main
 
 import (
 	"demo1/internal/app/mydemo/service"
-	"encoding/csv"
+	"fmt"
 	"os"
-	"strconv"
 	"time"
 
 	"go.uber.org/zap"
@@ -33,8 +32,8 @@ func main() {
 
 	}
 
-	timeday := time.Now()
-	today := timeday.Year()*10000 + int(timeday.Month())*100 + timeday.Day()
+	// timeday := time.Now()
+	// today := timeday.Year()*10000 + int(timeday.Month())*100 + timeday.Day()
 	yestodaytime := time.Now().AddDate(0, 0, -1)
 	yestoday := yestodaytime.Year()*10000 + int(yestodaytime.Month())*100 + yestodaytime.Day()
 
@@ -43,7 +42,11 @@ func main() {
 	// 	service.Logger.Error("GetCreateUidFromUserCheckinJoin err", zap.Error(err))
 	// }
 
-	userCheckinJoins, err := service.GetCreateUidCidFromUserCheckinJoin()
+	page := 1
+	pagesize := 3
+	isasc := true
+	//分页查
+	userCheckinJoins, err := service.GetCreateUidCidFromUserCheckinJoin(page, pagesize, isasc)
 	if err != nil {
 		service.Logger.Error("GetCreateUidCidFromUserCheckinJoin err", zap.Error(err))
 	}
@@ -51,37 +54,43 @@ func main() {
 	service.Logger.Info("uids", zap.Any("uidcid", userCheckinJoins))
 
 	//创建csv
-	file, err := os.Create("two_day_not_checkin.csv")
+	//file, err := os.Create("two_day_not_checkin.csv")
+	file, err := os.Create("two_day_not_checkin.txt")
 	if err != nil {
 		service.Logger.Error("Create err", zap.Error(err))
 	}
 	defer file.Close()
 
-	//初始化
-	writer := csv.NewWriter(file)
-	defer writer.Flush()
+	// //初始化
+	// // txt 自己写"\t"分隔 "\n"换行
+	// writer := csv.NewWriter(file)
+	// defer writer.Flush()
+	// // 写入
+	// err = writer.Write([]string{
+	// 	"uid",
+	// 	"cid",
+	// })
+	// if err != nil {
+	// 	service.Logger.Error("Write err", zap.Error(err))
+	// }
 
-	// 写入
-	writer.Write([]string{
-		"uid",
-		"cid",
-	})
+	num, err := file.WriteString("uid\tcid\n")
+	if err != nil {
+		service.Logger.Error("WriteString err", zap.Error(err))
+	}
+	service.Logger.Info("num", zap.Int("num", num))
 
 	for _, v := range userCheckinJoins {
-		userCheckinRecord1, err := service.GetUserCheckinRecord(v.Uid, v.Cid, today)
-		if err != nil {
-			service.Logger.Error("GetUserCheckinRecord1 err", zap.Error(err))
-		}
-		userCheckinRecord2, err := service.GetUserCheckinRecord(v.Uid, v.Cid, yestoday)
+		//date >= <= 限制时间范围
+		//select count
+		count, err := service.GetUserCheckinRecordTwoDayCount(v.Uid, v.Cid, yestoday)
 		if err != nil {
 			service.Logger.Error("GetUserCheckinRecord1 err", zap.Error(err))
 		}
 
-		if userCheckinRecord1 == nil && userCheckinRecord2 == nil {
-			writer.Write([]string{
-				strconv.Itoa(v.Uid),
-				strconv.Itoa(v.Cid),
-			})
+		if count == 0 {
+			str := fmt.Sprintf("%d\t%d\n", v.Uid, v.Cid)
+			file.WriteString(str)
 		}
 	}
 
